afdae55ffe02886b5d1d3a401057dfe736b02305
Mustafa Ahmed Raza <mustafaahmedraza@gmail.com>
2021-09-14 21:12:09 +0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_example/Pages/splash_page.dart';
+
+void main() {
+  runApp(SplashPage(key: UniqueKey(), onInitializationComplete: null));
+}
